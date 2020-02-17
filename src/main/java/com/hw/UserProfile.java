888746193531cba125a2b0714a1ef3a9c8b2a11b
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-02-16 19:17:20 -0500

    update shared content

--- src/main/java/com/hw/UserProfile.java
+++ src/main/java/com/hw/UserProfile.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class UserProfile {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(UserProfile.class, args);
     }
 }
