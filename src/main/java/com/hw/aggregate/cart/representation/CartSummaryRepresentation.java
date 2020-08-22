74139d7d962773b7263787de700a16be922c7de5
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-08-22 12:57:40 -0400

    fix cart attr sale order issue

--- src/main/java/com/hw/aggregate/cart/representation/CartSummaryRepresentation.java
+++ src/main/java/com/hw/aggregate/cart/representation/CartSummaryRepresentation.java
@@ -1,9 +1,11 @@
 package com.hw.aggregate.cart.representation;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.hw.aggregate.cart.model.CartItem;
 import com.hw.aggregate.order.model.BizOrderItemAddOn;
 import lombok.Data;
 
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -26,6 +28,7 @@ public class CartSummaryRepresentation {
         private String finalPrice;
         private String imageUrlSmall;
         private String productId;
+        @JsonDeserialize(as= LinkedHashSet.class)//use linkedHashSet to keep order of elements as it is received
         private Set<String> attributesSales;
         private Map<String,String> attrIdMap;
 
