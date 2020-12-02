af8459440fcdfe7ba0da7ee4974b79b1344f5b15
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-12-01 21:54:43 -0500

    simplify service

--- src/main/java/com/hw/aggregate/order/model/BizOrderQueryRegistry.java
+++ src/main/java/com/hw/aggregate/order/model/BizOrderQueryRegistry.java
@@ -9,11 +9,4 @@ public class BizOrderQueryRegistry extends RestfulQueryRegistry<BizOrder> {
     public Class<BizOrder> getEntityClass() {
         return BizOrder.class;
     }
-    private void setUp() {
-        cacheable.put(RoleEnum.USER, true);
-        cacheable.put(RoleEnum.ADMIN, true);
-        cacheable.put(RoleEnum.APP, true);
-        cacheable.put(RoleEnum.PUBLIC, true);
-        cacheable.put(RoleEnum.ROOT, true);
-    }
 }
